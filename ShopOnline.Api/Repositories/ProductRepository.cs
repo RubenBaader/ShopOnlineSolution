0566309bea8cfaea4da0e493bfdb1a8999b9cb67
RubenBaader <rubenbaader@gmail.com>
2023-11-28 11:26:30 +0100

    Implemented data transfer objects and enabled database read on HTTP GET request

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.ShopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        public ShopOnlineDbContext ShopOnlineDbContext { get; }
+
+        public Task<ProductCategory> GetCategorie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.ShopOnlineDbContext.ProductCategories.ToListAsync();
+
+            return categories;
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await ShopOnlineDbContext.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
