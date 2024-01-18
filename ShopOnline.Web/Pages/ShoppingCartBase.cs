ad1d5528e5ae286618faa9ccf4584bf2b0c3a6c7
RubenBaader <rubenbaader@gmail.com>
2024-01-18 09:18:04 +0100

    Added functionality to update qty in shopping cart, hide UpdateQtyButton when not needed and show on item count change

--- ShopOnline.Web/Pages/ShoppingCartBase.cs
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 
@@ -7,17 +8,23 @@ namespace ShopOnline.Web.Pages
     public class ShoppingCartBase : ComponentBase
     {
         [Inject]
+        public IJSRuntime Js {  get; set; }
+        [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
 
         public List<CartItemDto> ShoppingCartItems { get; set; }
 
         public string ErrorMessage { get; set; }
 
+        protected string TotalPrice { get; set; }
+        protected int TotalQuantity { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             try
             {
                 ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+                CalculateCartSummaryTotals();
             }
             catch (Exception Ex)
             {
@@ -30,10 +37,78 @@ namespace ShopOnline.Web.Pages
             var cartItemDto = await ShoppingCartService.DeleteItem(id);
 
             RemoveCartItem(id);
-
+            CalculateCartSummaryTotals();
         }
 
-        private CartItemDto GetCartItem (int id)
+        protected async Task UpdateQtyCartItem_Click(int id, int qty)
+        {
+            try
+            {
+                if (qty > 0)
+                {
+                    var updateItemDto = new CartItemQtyUpdateDto
+                    {
+                        CartItemId = id,
+                        Qty = qty,
+                    };
+
+                    var returnedUpdateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
+
+                    UpdateTotalItemPrice(returnedUpdateItemDto);
+                    CalculateCartSummaryTotals();
+                    await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, false);
+                }
+                else
+                {
+                    var item = this.ShoppingCartItems.FirstOrDefault(i => i.Id == id);
+                    if (item != null)
+                    {
+                        item.Qty = 1;
+                        item.ToTalPrice = item.Price;
+                    }
+                }
+
+                //in all cases, update cart summary totals and hide update button
+                //CalculateCartSummaryTotals();
+                //await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, false);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected async Task UpdateQty_Input(int id)
+        {
+            //await Js.InvokeVoidAsync("ClickTestHelloWorld");
+            await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, true);
+        }
+
+        private void CalculateCartSummaryTotals()
+        {
+            SetTotalPrice();
+            SetTotalQuantity();
+        }
+
+        private void UpdateTotalItemPrice(CartItemDto cartItemDto)
+        {
+            var item = GetCartItem(cartItemDto.Id);
+
+            if (item != null)
+            {
+                item.ToTalPrice = cartItemDto.Price * cartItemDto.Qty;
+            }
+        }
+        private void SetTotalPrice()
+        {
+            TotalPrice = this.ShoppingCartItems.Sum(p => p.ToTalPrice).ToString("C");
+        }
+        private void SetTotalQuantity()
+        {
+            TotalQuantity = this.ShoppingCartItems.Sum(p => p.Qty);
+        }
+        private CartItemDto GetCartItem(int id)
         {
             return ShoppingCartItems.FirstOrDefault(i => i.Id == id);
         }
