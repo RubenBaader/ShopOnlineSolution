df69877db3f45be63ed61c38d0a2428c172e7fe5
RubenBaader <rubenbaader@gmail.com>
2024-01-29 12:40:42 +0100

    Added functionality to browser products by category via sidebar selection. Changed favicon and added site icon.

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -92,5 +92,24 @@ namespace ShopOnline.Api.Controllers
                                 "Error retrieving data from database");
             }
         }
+
+        [HttpGet]
+        [Route("{categoryId}/GetItemsByCategory")]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItemsByCategory(int categoryId)
+        {
+            try
+            {
+                var products = await productRepository.GetItemsByCategory(categoryId);
+                var productCategories = await productRepository.GetCategories();
+                var productDtos = products.ConvertToDto(productCategories);
+
+                return Ok(productDtos);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Error retrieving data from database");
+            }
+        }
     }
 }
